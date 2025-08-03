d5a68ffeefb3c093e3efb58ce43d037dd12bdfe5
Debisi-o <debisioyewole@gmail.com>
2025-08-03 16:33:42 -0400

    Add view and edit functionality for blog posts

---
+++ script/postPage.js
@@ -0,0 +1,29 @@
+import { getPosts, savePosts, getPostById } from '../utils/storage.js';
+
+const urlParams = new URLSearchParams(window.location.search);
+const postId = urlParams.get('id');
+const post = getPostById(postId);
+
+if (!post) {
+    document.getElementById('postContainer').innerHTML = "Post not found!";
+} else {
+    document.getElementById('editTitle').value = post.title;
+    document.getElementById('editContent').value = post.content;
+}
+
+document.getElementById('updateBtn').onclick = () => {
+    const updatedTitle = document.getElementById('editTitle').value;
+    const updatedContent = document.getElementById('editContent').value;
+
+    const posts = getPosts();
+    const index = posts.findIndex(p => p.id === postId);
+    posts[index] = { ...posts[index], title: updatedTitle, content: updatedContent };
+    savePosts(posts);
+    alert("Post updated!");
+};
+
+// document.getElementById('deleteBtn').onclick = () => {
+//     const posts = getPosts().filter(p => p.id !== postId);
+//     savePosts(posts);
+//     window.location.href = "index.html";
+// };
